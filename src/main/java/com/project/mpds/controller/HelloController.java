30f40d46717aad0b5182f66eacd56da69d3a19de
rainful <1690030129@qq.com>
2022-02-18 21:18:43 +0800

    feat: 增加解释

--- src/main/java/com/project/mpds/controller/HelloController.java
+++ src/main/java/com/project/mpds/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.project.mpds.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Rainful
@@ -13,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/world")
+    @GetMapping("/world") // todo 1
+    /* 这里我们确定好请求方式 总计四种 get / post / put / delete 分别对应 @GetMapping / @PostMapping / @PutMapping / @DeleteMapping */
+    /*
+        括号里面代表路径 最上面有个 requestMapping 代表层级目录 所以这个方法的地址就是 /hello/world
+        稍后启动之后可以访问浏览器 localhost:8080/hello/world 就可以看到hello world了
+     */
     public String helloWorld(){
+        // todo 2 在这里写一个 public 方法 返回的类型先写字符串吧 String
         return "Hello World";
     }
+
+
 }
